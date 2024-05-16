b8c2c4d7468154540378f54f7a834a7175340d9d
ThanhDN Irtech <thanhdn@irtech.com.vn>
2024-05-16 18:22:39 +0700

    Update queue and composer lock

--- app/Jobs/SendAutomaticNotification.php
+++ app/Jobs/SendAutomaticNotification.php
@@ -4,12 +4,13 @@ namespace App\Jobs;
 
 use App\Http\Traits\Helpers\NotificationTrait;
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 
-class SendAutomaticNotification implements ShouldQueue
+class SendAutomaticNotification implements ShouldQueue, ShouldBeUnique
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels, NotificationTrait;
 
