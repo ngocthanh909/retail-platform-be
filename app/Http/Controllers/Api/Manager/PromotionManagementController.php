77a8c7cf488f70f60287f0a1d62f7593bbfe79e6
ThanhDN Irtech <thanhdn@irtech.com.vn>
2024-05-27 13:58:12 +0700

    Promotion customer - Add product gift info

--- app/Http/Controllers/Api/Manager/PromotionManagementController.php
+++ app/Http/Controllers/Api/Manager/PromotionManagementController.php
@@ -44,7 +44,7 @@ class PromotionManagementController extends Controller
     public function detail(Request $request, $id)
     {
         try {
-            $promotion = Promotion::with(['applyProduct', 'applyProduct.product', 'applyProduct.category', 'applyCustomer.customer'])->findOrFail($id);
+            $promotion = Promotion::with(['applyProduct', 'applyProduct.product', 'applyProduct.category', 'applyCustomer.customer', 'giftProduct'])->findOrFail($id);
             return $this->success($promotion);
         } catch (\Throwable $e) {
             Log::error($e);
